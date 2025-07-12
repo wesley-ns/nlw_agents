5988aca4ba11d53622b9a8553d0f8707a6d084f7
Wesley Sehn <wesleysehn19@gmail.com>
2025-07-12 20:34:31 -0300

    feat: add questions system and reorganize routes
    - Add questions schema to database
    - Create migration for questions table
    - Implement routes for creating questions and rooms
    - Reorganize route structure in routes/ directory
    - Update database seed
    - Modify server and HTTP client configurations

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,10 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
-import { getRoomsRoute } from './http/get-rooms.ts';
+import { createQuestionRoute } from './http/routes/create-question.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -22,6 +25,9 @@ app.get('/health', () => {
 });
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomQuestionsRoute);
+app.register(createQuestionRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   // HTTP server running!
